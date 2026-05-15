0c33030f3611d4f5a7a8d8b8652fadef0b40962a
Dheeraj Saini <115359474+Ds10011003@users.noreply.github.com>
2026-05-15 11:58:32 +0530

    Updated in typing animation

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [ "Developer", "Data Analyst", "Web Designer"],
+        strings: ["Data Analyst 1", "Salesforce Professional","Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
